99206f34277910986ba41f5cd6ad09f99c5af79e
Kenan Poole <kenanpoole@gmail.com>
2019-12-02 06:04:15 -0500

    updated index, css, js

---
+++ script.js
@@ -0,0 +1,13 @@
+//check for Navigation Timing API support
+if (window.performance) 
+{
+    console.info("window.performance works fine on this browser");
+}
+    
+if (performance.navigation.type == 1) 
+{
+    console.info( "This page is reloaded" );
+}else 
+{
+    console.info( "This page is not reloaded");
+}
